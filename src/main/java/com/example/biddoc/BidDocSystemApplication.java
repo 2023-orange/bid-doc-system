0c82b152ae5bdd62b4642e76b97a668bfa2eda35
LiuXing <lgl1305174214@gmail.com>
2026-01-27 22:26:02 +0800

    新建项目资质库管理系统

---
+++ src/main/java/com/example/biddoc/BidDocSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.biddoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidDocSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BidDocSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/biddoc/BidDocSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.biddoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidDocSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
